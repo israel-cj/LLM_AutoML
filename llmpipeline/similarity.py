2203f274da67e15aa5faeb7324e086245d2b6c91
israel-cj <93375286+israel-cj@users.noreply.github.com>
2023-08-13 10:00:43 +0200

    Change from data.json to data_automlbenchmark.py

--- llmpipeline/similarity.py
+++ llmpipeline/similarity.py
@@ -1,6 +1,6 @@
-import json
 import requests
 import random
+from .data_automlbenchmark import data_automl
 
 def TransferedPipelines(hf_token, name_dataset=None, number_of_pipelines=5):
     API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/msmarco-distilbert-base-tas-b"
@@ -10,11 +10,8 @@ def TransferedPipelines(hf_token, name_dataset=None, number_of_pipelines=5):
         response = requests.post(API_URL, headers=headers, json=payload)
         return response.json()
     
-
-    # Open the JSON file
-    with open('data.json', 'r') as f:
-        # Load the JSON data which contains the pipelines from the AutoMLBenchmark
-        automl_benchmark_data = json.load(f)
+    # Load the JSON data which contains the pipelines from the AutoMLBenchmark
+    automl_benchmark_data = data_automl
         
     list_names_datasets = list(automl_benchmark_data.keys())
     
